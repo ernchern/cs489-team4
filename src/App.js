73b4493b394a1762188f83690b0e950240e44aae
Jeongeon Park <patricia021912@gmail.com>
2019-11-05 09:44:44 +0900

    Initialize reactJS

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import './components/middleSection';
+import middleSection from './components/middleSection';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Header</h1>
+        <p>
+          Contents
+        </p>
+        <p>
+          We still need to decide between material UI and bootstrap
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/middleSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MiddleSection extends React.Component {
+    render() {
+    return (
+        <div>helloworld</div>
+    );
+    }
+
+}
+
+export default MiddleSection;
