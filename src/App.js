91117fb41baf6d5d83398ada4f0a6c8a62dbb4a8
Ern Chern <39647817+ernchern@users.noreply.github.com>
2019-11-10 00:43:09 +0900

    Update App.js
    add firebase component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import './components/middleSection';
-import middleSection from './components/middleSection';
+
+// require firebase
+const firebase = require('firebase');
+
+// reuire uuid
+const uuid = require('uuid');
+
+const config = {
+apiKey: "AIzaSyDX2D4nRHz6jFAfQRCRBawGcWdqOxMEFNk",
+    authDomain: "cs489-team-4.firebaseapp.com",
+    databaseURL: "https://cs489-team-4.firebaseio.com",
+    projectId: "cs489-team-4",
+    storageBucket: "cs489-team-4.appspot.com",
+    messagingSenderId: "72131328035",
+    appId: "1:72131328035:web:b0ec224f6212453c1d9996",
+    measurementId: "G-P84YC6N7BK"
+  };
+firebase.initializeApp(config);
 
 function App() {
   return (
