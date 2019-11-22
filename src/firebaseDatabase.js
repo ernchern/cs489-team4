d98fab15beef5e5c8c57ae7b9cd7b4e8d04e3585
doxuanviet <doxuanviet1996@kaist.ac.kr>
2019-11-22 22:55:03 +0900

    add firebase

---
+++ src/firebaseDatabase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOUd1lmmjMlPqVrg5HztvYWbAAV-cjIYo",
+  authDomain: "cs489-team-4.firebaseapp.com",
+  databaseURL: "https://cs489-team-4.firebaseio.com",
+  projectId: "cs489-team-4",
+  storageBucket: "cs489-team-4.appspot.com",
+  messagingSenderId: "72131328035",
+  appId: "1:72131328035:web:b0ec224f6212453c1d9996",
+  measurementId: "G-P84YC6N7BK"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
