a0e50b22180a8af3cf50388701e8b2a34007c640
Ern Chern <39647817+ernchern@users.noreply.github.com>
2019-11-22 18:19:38 +0900

    Update IntroPage.js

--- src/pages/Introduction/IntroPage.js
+++ src/pages/Introduction/IntroPage.js
@@ -8,9 +8,21 @@ export class IntroPage extends Component {
         return (
             <div class="overallLayout">
                 <div class="textCenter">
-                    
-                    
-                    <div>This is the Introduction Page</div>
+                   <div>
+                    Welcome to Moral Survey! Thank you for agreeing to take part in this survey.
+                    <br>
+                    <br>
+                    The purpose of our project is to study human's moral decision making process and help machine to make better moral decision for human.
+                    <br>
+                    <br>
+                    The procedure involves filling an online survey that will take approximately 5 to 10 minutes. The questions will be asking whether if you agree on the moral decisions in some certain situations. Note that there is no absolute answer and you can freely choose the answer based on your own opinion.
+                    <br>
+                    <br>
+                    Your responses will be confidential and we do not collect identifying information such as your name, email address or IP address.
+                    <br>
+                    <br>
+                    Clicking on the start button below indicates that you have read the above information and voluntarily agree to participate.
+                    </div>
                     <Button href="basicinfo" variant="primary" type="submit">Start</Button>
                 
                 </div>
