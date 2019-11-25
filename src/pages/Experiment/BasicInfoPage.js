5fbc90cbb9449d706eed5deda1242af7d975ccd5
ernchern <39647817+ernchern@users.noreply.github.com>
2019-11-26 00:16:21 +0900

    change back the age

--- src/pages/Experiment/BasicInfoPage.js
+++ src/pages/Experiment/BasicInfoPage.js
@@ -26,15 +26,10 @@ export class BasicInfoPage extends Component {
                         <Form.Group controlId="formAge">
                             <Form.Label style={{marginRight: "15px"}}>Age</Form.Label>
                             <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-                                <ToggleButton variant="outline-primary" value={1}>14 and below</ToggleButton>
-                                <ToggleButton variant="outline-primary" value={2}>15-19</ToggleButton>
-                                <ToggleButton variant="outline-primary" value={3}>20-24</ToggleButton>
-                                <ToggleButton variant="outline-primary" value={4}>25-29</ToggleButton>
-                                <ToggleButton variant="outline-primary" value={5}>30-34</ToggleButton>
-                                <ToggleButton variant="outline-primary" value={6}>35-39</ToggleButton>
-                                <ToggleButton variant="outline-primary" value={7}>40-44</ToggleButton>
-                                <ToggleButton variant="outline-primary" value={8}>45-49</ToggleButton>
-                                <ToggleButton variant="outline-primary" value={9}>50 and above</ToggleButton>
+                                <ToggleButton variant="outline-primary" value={1}>19 and below</ToggleButton>
+                                <ToggleButton variant="outline-primary" value={2}>20-29</ToggleButton>
+                                <ToggleButton variant="outline-primary" value={3}>30-39</ToggleButton>
+                                <ToggleButton variant="outline-primary" value={4}>40 and above</ToggleButton>
                             </ToggleButtonGroup>
                         </Form.Group>
 
