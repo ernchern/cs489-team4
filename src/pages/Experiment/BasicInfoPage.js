9cb32a556de5d8769ae778b613b756fb1d8da90a
ernchern <39647817+ernchern@users.noreply.github.com>
2019-11-26 01:09:32 +0900

    pass form value to this.state

--- src/pages/Experiment/BasicInfoPage.js
+++ src/pages/Experiment/BasicInfoPage.js
@@ -30,31 +30,28 @@ export class BasicInfoPage extends Component {
                     <Form>
                         <Form.Group controlId="formGender">
                             <Form.Label style={{marginRight: "15px"}}>Gender</Form.Label>
-                            <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-                                <ToggleButton variant="outline-primary" value={1}>Male</ToggleButton>
-                                <ToggleButton variant="outline-primary" value={2}>Female</ToggleButton>
-                                <ToggleButton variant="outline-primary" value={3}>Rather not to say</ToggleButton>
+                            <ToggleButtonGroup type="radio" name="options" onChange={(val) => this.setState({gender:val})}>
+                                <ToggleButton variant="outline-primary" value={'male'}>Male</ToggleButton>
+                                <ToggleButton variant="outline-primary" value={'female'}>Female</ToggleButton>
+                                <ToggleButton variant="outline-primary" value={'none'}>Rather not to say</ToggleButton>
                             </ToggleButtonGroup>
                         </Form.Group>
-
                         <Form.Group controlId="formAge">
                             <Form.Label style={{marginRight: "15px"}}>Age</Form.Label>
-                            <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-                                <ToggleButton variant="outline-primary" value={1}>19 and below</ToggleButton>
-                                <ToggleButton variant="outline-primary" value={2}>20-29</ToggleButton>
-                                <ToggleButton variant="outline-primary" value={3}>30-39</ToggleButton>
-                                <ToggleButton variant="outline-primary" value={4}>40 and above</ToggleButton>
+                            <ToggleButtonGroup type="radio" name="options" onChange={(val) => this.setState({age:val})} >
+                                <ToggleButton variant="outline-primary" value={"<19"}>19 and below</ToggleButton>
+                                <ToggleButton variant="outline-primary" value={"20-29"}>20-29</ToggleButton>
+                                <ToggleButton variant="outline-primary" value={"30-39"}>30-39</ToggleButton>
+                                <ToggleButton variant="outline-primary" value={">40"}>40 and above</ToggleButton>
                             </ToggleButtonGroup>
                         </Form.Group>
-
                         <Form.Group controlId="formCSBackground">
                             <Form.Label style={{marginRight: "15px"}}>CS Background</Form.Label>
-                            <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-                                <ToggleButton variant="outline-primary" value={1}>Yes</ToggleButton>
-                                <ToggleButton variant="outline-primary" value={2}>No</ToggleButton>
+                            <ToggleButtonGroup type="radio" name="options" onChange={(val) => this.setState({cs:val})}>
+                                <ToggleButton variant="outline-primary" value={"yes"}>Yes</ToggleButton>
+                                <ToggleButton variant="outline-primary" value={"no"}>No</ToggleButton>
                             </ToggleButtonGroup>
                         </Form.Group>
-
                         <Form.Group controlId="formNationality">
                             <Form.Label style={{marginRight: "15px"}}>Nationality</Form.Label>
 							<CountryDropdown value={this.state.nationality} onChange={(val) => this.setState({nationality:val})}/>
@@ -62,7 +59,7 @@ export class BasicInfoPage extends Component {
                         
                     </Form>
                     <div className="buttonPadding">
-                    <Button href="questions" variant="primary" onClick={()=>this.props.callbackFromParent("18")} type="submit">
+                    <Button href="questions" variant="primary" onClick={()=>this.props.callbackFromParent(this.state.age)} type="submit">
                         Click to Proceed
                     </Button>
                     </div>
