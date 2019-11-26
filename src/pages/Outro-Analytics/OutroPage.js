4ac6cf7e959985df496a2add7fb2c5313ed9a465
ernchern <39647817+ernchern@users.noreply.github.com>
2019-11-27 00:35:22 +0900

    outro

--- src/pages/Outro-Analytics/OutroPage.js
+++ src/pages/Outro-Analytics/OutroPage.js
@@ -8,7 +8,24 @@ export class OutroPage extends Component {
         return (
             <div class="overallLayout">
                 <div class="textCenter">
-                    <div>This is the Outro Page</div>                    
+                    <div>
+					Thank you so much for participating in the survey!
+					<br/>
+					<br/>
+					Moral Survey is actually a blinded experiment. Two sets of questions are randomly assigned to our participants. One tells the participants that moral decisions are all made by machine, while the other one tells that some humans have made the same moral decisions. You had only answered one of the question sets.
+					<br/>
+					<br/>
+					The purpose of this experiment is to study if human has any bias on moral decisions made by machine, that human may change their opinion, depending on who has made the decisions.
+					<br/>
+					<br/>
+					This is an important experiment to measure the level of trust of human on machine, before we discuss how machine should make their moral decisions. The machine's decisions will only have its significant meaning, if human trusts its decisions, as much as other human's decisions.
+					<br/>
+					<br/>
+					If you are interested in our experiment or you are curious about the final result analysis, please tell us.
+					<br/>
+					<br/>
+					Thank you again for your support and participation. :)
+					</div>                    
                 </div>
                 <br/>
                 <div class="buttonPosition">
